bf1c7bf597cf40040c028c8dd6c4fadc0dbf76e1
Besong Wisdom <engineerbesong@gmail.com>
2025-09-10 20:40:29 +0100

    Fix server error: remove static serving from main/urls.py and secure email settings

--- main/urls.py
+++ main/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
 
 # Add this at the end to serve media files during development
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
